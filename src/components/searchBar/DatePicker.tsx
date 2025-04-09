01eb17b607fd93a1f88c41ad27f30b0f423b0df2
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-04-09 13:32:05 +0200

    :sparkles: add function to check value on date

--- src/components/searchBar/DatePicker.tsx
+++ src/components/searchBar/DatePicker.tsx
@@ -1,9 +1,16 @@
 import { StyledDateInput } from "../styled/StyledInputs";
 
 export const DatePicker = () => {
+  const handleDate = (e) => {
+    console.log(e.target.valueAsNumber);
+  };
   return (
     <>
-      <StyledDateInput />
+      <StyledDateInput
+        onChange={(e) => {
+          handleDate(e);
+        }}
+      />
     </>
   );
 };
