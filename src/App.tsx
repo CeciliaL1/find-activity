b28b068027933e2383074d7e9d1a395b3560d90e
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-04-10 10:59:18 +0200

    :sparkles: add searchContext to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { RouterProvider } from "react-router";
 import "./App.css";
 import { Router } from "./Router";
+import { SearchContext } from "./context/SearchContext";
+import { useReducer } from "react";
+import { SearchReducer } from "./reducer/SearchReducer";
 
 function App() {
+  const [search, searchDispatch] = useReducer(SearchReducer, {
+    age: [0, 100],
+    date: "",
+    price: [0, 1000],
+    checks: [],
+  });
   return (
     <>
-      <RouterProvider router={Router}></RouterProvider>
+      <SearchContext.Provider value={{ search, searchDispatch }}>
+        <RouterProvider router={Router}></RouterProvider>
+      </SearchContext.Provider>
     </>
   );
 }
