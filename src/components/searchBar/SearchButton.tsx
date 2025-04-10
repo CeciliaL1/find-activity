0cea2152451ce073e11bd319b2586f139f4f65c9
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-04-10 10:58:52 +0200

    :sparkles: add search to context

--- src/components/searchBar/SearchButton.tsx
+++ src/components/searchBar/SearchButton.tsx
@@ -1,21 +1,28 @@
+import { useContext } from "react";
 import { getLocalStorage } from "../../helperfunctions/getLocalStorage";
 import { IChecks } from "../../models/ISearch";
 import { StyledSearchButton } from "../styled/StyledButtons";
+import { SearchContext, SearchEnum } from "../../context/SearchContext";
 
 export const SearchButton = () => {
+  const { searchDispatch } = useContext(SearchContext);
+
   const handleSearch = () => {
     const age: number[] = getLocalStorage<number[]>("age");
     let date: string = getLocalStorage<string>("date");
     const price: number[] = getLocalStorage<number[]>("price");
     const checks: IChecks[] = getLocalStorage<IChecks[]>("checks");
-    if (!date || Array.isArray(date) || isNaN(Number(date))) {
+    if (!date || Array.isArray(date)) {
       date = new Date().toDateString();
     }
 
-    console.log("age", age);
-    console.log("date", date);
-    console.log("price", price);
-    console.log("checks", checks);
+    const newSearch = {
+      age,
+      date,
+      price,
+      checks,
+    };
+    searchDispatch({ type: SearchEnum.SEARCH, payload: newSearch });
   };
   return (
     <>
