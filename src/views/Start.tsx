f0b9fce0579464a9835a0548a5c4ad08ff179f89
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-05-14 08:38:12 +0200

    :sparkles: add link to favorites when search

--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -15,8 +15,7 @@ import { getActivitiesSun } from "../helperfunctions/getActivitiesSun";
 import { getActivitiesRain } from "../helperfunctions/getActivitiesRain";
 import { RenderActivities } from "../components/RenderActivities";
 import { ActivitiesEnum, ActivityContext } from "../context/ActivitiesContext";
-import { Favorites } from "./Favorites";
-import { Link, NavLink } from "react-router";
+import { Link } from "react-router";
 
 export const Start = () => {
   const { search } = useContext(SearchContext);
@@ -141,7 +140,8 @@ export const Start = () => {
 
   return (
     <>
-      <Link to="/favorites">Favoriter</Link>
+      {search.search && <Link to="/favorites">Favoriter</Link>}
+
       <h5>
         {search.search
           ? loadingWeather
